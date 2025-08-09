7fea4b352eac3b13f90aba03664cbf777d1dc6c2
Shaan2170 <shaanmustafa1234@gmail.com>
2025-08-09 20:20:40 +0530

    Skeleton routes for user

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
+mongoose.connect()
+
+const app = express();
+app.use(express.json());
+
+app.post("/user/signup", async function (req,res){
+    
+})
+
+app.post("/user/signin", async function (req,res){
+    
+})
+
+app.get("/user/purchases", async function (req,res){
+    
+})
+
+app.get("/user/course/purchase", async function (req,res){
+    
+})
+
+app.get("/courses", function (req,res){
+
+})
