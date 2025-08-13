a8570adfc31cff22dd5eae9316e2f8e9abe04fc4
Shaan2170 <shaanmustafa1234@gmail.com>
2025-08-13 17:52:41 +0530

    Added auth endpoints

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,48 @@
 const { Router } = require("express");
+const { userModel } =require("../db");
+const jwt = require("jsonwebtoken");
+const JWT_USER_PASSWORD = "sbkbakbd123";
 
 const userRouter = Router();
 
-userRouter.post("/signup", function(req, res) {
+userRouter.post("/signup", async function(req, res) {
+    const {email, password , firstName, lastName } = req.body;
+    
+    
+    await userModel.create({
+        email: email,
+        password: password,
+        firstName,
+        lastName
+
+    })
     res.json({
-        message: "signup endpoint"
+        message: "Signup succeeded"
     })
 })
 
-userRouter.post("/signin", function(req, res) {
-    res.json({
-        message: "signup endpoint"
+userRouter.post("/signin", async function(req, res) {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({
+        email: email,
+        password: password
+    });
+
+    if (user) {
+        jwt.sign ({
+            id: user._id
+        }, JWT_USER_PASSWORD);
+
+        res.json({
+            token: token
+        })
+    } else {
+        res.json({
+        message: "Incorrect credentials"
     })
-})
+}}
+)
 
 userRouter.get("/purchases", function(req, res) {
     res.json({
